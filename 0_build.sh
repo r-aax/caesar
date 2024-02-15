6e6d3ae1cd7cdc67f51bd546335bc7947c56b9a2
Alexey Rybakov <rybakov@jscc.ru>
2024-02-15 18:13:11 +0300

    #16 : build improve

--- 0_build.sh
+++ 0_build.sh
@@ -1,58 +1,110 @@
-#!/bin/sh
+#!/bin/bash
 
-set -x
+# set -x
 
+# Mode.
+MODE="$1"
+if [ -z "$MODE" ]
+then
+    MODE="all"
+fi
+
+# Solver options.
 SOLVER="empty"
 SH="src/solver/solver.h"
 
+# Build flags.
 FLG="-O3 -std=c++20"
 INC="-I./src"
 SOLVER_SRC="src/solver_${SOLVER}"
 SRC="src/geom/*.cpp src/mesh/*.cpp src/mth/*.cpp src/utils/*.cpp ${SOLVER_SRC}/*.cpp"
 LIB="-lm"
 
-rm -rf caesar caesar_d doc/html caesar_test
+# Clear.
+function clear()
+{
+    rm -rf caesar caesar_d doc/html caesar_test
+}
 
-# Create solver include.
-echo "/// \\\file"                                   > ${SH}
-echo "/// \\\brief Solver declaration."             >> ${SH}
-echo "///"                                          >> ${SH}
-echo "/// Solver declaration."                      >> ${SH}
-echo ""                                             >> ${SH}
-echo "#ifndef SOLVER_H"                             >> ${SH}
-echo "#define SOLVER_H"                             >> ${SH}
-echo ""                                             >> ${SH}
-echo "#include \"solver_empty/solver_${SOLVER}.h\"" >> ${SH}
-echo ""                                             >> ${SH}
-echo "#endif // SOLVER_H"                           >> ${SH}
-echo ""                                             >> ${SH}
+# Create solver incude.
+function create_solver_include()
+{
+    echo "/// \\\file"                                   > ${SH}
+    echo "/// \\\brief Solver declaration."             >> ${SH}
+    echo "///"                                          >> ${SH}
+    echo "/// Solver declaration."                      >> ${SH}
+    echo ""                                             >> ${SH}
+    echo "#ifndef SOLVER_H"                             >> ${SH}
+    echo "#define SOLVER_H"                             >> ${SH}
+    echo ""                                             >> ${SH}
+    echo "#include \"solver_empty/solver_${SOLVER}.h\"" >> ${SH}
+    echo ""                                             >> ${SH}
+    echo "#endif // SOLVER_H"                           >> ${SH}
+    echo ""                                             >> ${SH}
+}
 
-g++ \
-    $FLG $INC $SRC ./src/*.cpp $LIB \
-    -o caesar
+# Start build.
 
-if [ "$?" -ne 0 ]
+clear
+create_solver_include
+
+
+# Fast build.
+if [ "$MODE" = "fast" ] || [ "$MODE" = "all" ]
 then
-    echo "ERROR : Compilation failed (fast version)."
-    exit 1
+    echo ".... Build fast version of CAEsar    ...."
+
+    g++ \
+        $FLG $INC $SRC ./src/*.cpp $LIB \
+        -o caesar
+
+    if [ "$?" -ne 0 ]
+    then
+        echo "ERROR : Compilation failed (fast version)."
+        exit 1
+    fi
 fi
 
-g++ \
-    -DDEBUG \
-    $FLG $INC $SRC ./src/*.cpp $LIB \
-    -o caesar_d
-
-if [ "$?" -ne 0 ]
+# Debug build.
+if [ "$MODE" = "debug" ] || [ "$MODE" = "all" ]
 then
-    echo "ERROR : Compilation failed (debug version)."
-    exit 1
+    echo ".... Build debug version of CAEsar   ...."
+
+    g++ \
+        -DDEBUG \
+        $FLG $INC $SRC ./src/*.cpp $LIB \
+        -o caesar_d
+
+    if [ "$?" -ne 0 ]
+    then
+        echo "ERROR : Compilation failed (debug version)."
+        exit 1
+    fi
 fi
 
-doxygen doc/Doxyfile
+#Documentation.
+if [ "$MODE" = "doc" ]
+then
+    echo ".... Make documentation for CAEsar   ...."
 
-g++ \
-    -DDEBUG \
-    $FLG $INC $SRC ./test/unit_catch2/*.cpp $LIB \
-    -o caesar_test
+    doxygen doc/Doxyfile
+fi
+if [ "$MODE" = "all" ]
+then
+    echo ".... Make documentation for CAEsar   ...."
 
-./caesar_test
+    doxygen doc/Doxyfile &> /dev/null
+fi
+
+# Tests.
+if [ "$MODE" = "test" ] || [ "$MODE" = "all" ]
+then
+    echo ".... Build and run testing of CAEsar ...."
+
+    g++ \
+        -DDEBUG \
+        $FLG $INC $SRC ./test/unit_catch2/*.cpp $LIB \
+        -o caesar_test
+
+    ./caesar_test
+fi
