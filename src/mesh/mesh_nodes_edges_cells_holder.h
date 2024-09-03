932f21a8b8558c8b1c24a7a91b5db0138c9028ca
Alexey Rybakov <rybakov@jscc.ru>
2024-09-03 17:23:41 +0300

    #31 : class mesh::NodesEdgesCellsHolder

---
+++ src/mesh/mesh_nodes_edges_cells_holder.h
@@ -0,0 +1,38 @@
+/// \file
+/// \brief Nodes, edges and cells holder.
+///
+/// Class that holds nodes, edges and cells.
+
+#ifndef MESH_NODES_EDGES_CELLS_HOLDER_H
+#define MESH_NODES_EDGES_CELLS_HOLDER_H
+
+#include "mesh_nodes_holder.h"
+#include "mesh_edges_holder.h"
+#include "mesh_cells_holder.h"
+
+namespace caesar
+{
+
+namespace mesh
+{
+
+/// \addtogroup mesh
+/// @{
+
+/// \brief Nodes, edges, cells holder.
+///
+/// Class that holds all objects - nodes, edges, cells.
+class NodesEdgesCellsHolder
+    : public NodesHolder,
+      public EdgesHolder,
+      public CellsHolder
+{
+}
+
+/// @}
+
+}
+
+}
+
+#endif // !MESH_NODS_EDGES_CELLS_HOLDER_H
